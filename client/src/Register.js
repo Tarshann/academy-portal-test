e41751e2fcdb061ffe83c5fa6144a3f872a2f718
Tarshann <138724143+Tarshann@users.noreply.github.com>
2025-04-14 12:29:37 -0500

    Create Register.js

---
+++ client/src/Register.js
@@ -0,0 +1,17 @@
+<h3>Register</h3>
+
+<label>First Name</label>
+<input
+  type="text"
+  name="firstName"
+  value={form.firstName}
+  onChange={handleChange}
+/>
+
+<label>Last Name</label>
+<input
+  type="text"
+  name="lastName"
+  value={form.lastName}
+  onChange={handleChange}
+/>
